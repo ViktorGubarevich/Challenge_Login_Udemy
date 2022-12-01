14fed99ccc6c7c1f1790317c8944800084753a4f
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2022-12-01 17:06:37 +0300

    Used useRef() without useState().

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,24 +1,41 @@
+import ReactDOM from "react-dom";
 import Button from "../Button/Button";
 import Card from "../Card/Card";
 import styles from "./Modal.module.css";
 
-const Modal = ({ title, message, onCloseModal }) => {
-  return (
-    <div>
-      <div className={styles.backdrop} onClick={onCloseModal}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onCloseModal}>Закрыть</Button>
-        </footer>
-      </Card>
+const Backdrop = ({ onCloseModal }) => (
+  <div className={styles.backdrop} onClick={onCloseModal}></div>
+);
+
+const ErrorModal = ({ title, message, onCloseModal }) => (
+  <Card className={styles.modal}>
+    <header className={styles.header}>
+      <h2>{title}</h2>
+    </header>
+    <div className={styles.content}>
+      <p>{message}</p>
     </div>
-  );
-};
+    <footer className={styles.actions}>
+      <Button onClick={onCloseModal}>Закрыть</Button>
+    </footer>
+  </Card>
+);
+
+const Modal = ({ title, message, onCloseModal }) => (
+  <>
+    {ReactDOM.createPortal(
+      <Backdrop onCloseModal={onCloseModal} />,
+      document.getElementById("backdrop")
+    )}
+    {ReactDOM.createPortal(
+      <ErrorModal
+        title={title}
+        message={message}
+        onCloseModal={onCloseModal}
+      />,
+      document.getElementById("modal")
+    )}
+  </>
+);
 
 export default Modal;
--- src/components/Users/NewUser/UserForm.js
+++ src/components/Users/NewUser/UserForm.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import Button from "../../UI/Button/Button";
 import Card from "../../UI/Card/Card";
@@ -6,20 +6,26 @@ import Modal from "../../UI/Modal/Modal";
 import styles from "./UserForm.module.css";
 
 const UserForm = ({ onSaveUserData }) => {
-  const [inputName, setInputName] = useState("");
-  const [inputAge, setInputAge] = useState("");
+  const inputNameRef = useRef();
+  const inputAgeRef = useRef();
   const [error, setError] = useState();
 
-  const nameChangeHandler = (event) => {
-    setInputName(event.target.value);
-  };
+  // const [inputName, setInputName] = useState("");
+  // const [inputAge, setInputAge] = useState("");
 
-  const ageChangeHandler = (event) => {
-    setInputAge(event.target.value);
-  };
+  // const nameChangeHandler = (event) => {
+  //   setInputName(event.target.value);
+  // };
+
+  // const ageChangeHandler = (event) => {
+  //   setInputAge(event.target.value);
+  // };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    const inputName = inputNameRef.current.value;
+    const inputAge = inputAgeRef.current.value;
 
     if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
       setError({
@@ -43,9 +49,11 @@ const UserForm = ({ onSaveUserData }) => {
     };
 
     onSaveUserData(userData);
+    inputNameRef.current.value = "";
+    inputAgeRef.current.value = "";
 
-    setInputName("");
-    setInputAge("");
+    // setInputName("");
+    // setInputAge("");
   };
 
   const errorHandler = () => {
@@ -54,18 +62,30 @@ const UserForm = ({ onSaveUserData }) => {
 
   return (
     <>
-      {error && <Modal title={error.title} message={error.message} onCloseModal={errorHandler} />}
+      {error && (
+        <Modal
+          title={error.title}
+          message={error.message}
+          onCloseModal={errorHandler}
+        />
+      )}
       <Card>
         <form onSubmit={formSubmitHandler}>
           <div className={styles["form-control"]}>
             <label>Имя</label>
-            <input type="text" value={inputName} onChange={nameChangeHandler} />
+            <input
+              type="text"
+              ref={inputNameRef}
+              // value={inputName}
+              // onChange={nameChangeHandler}
+            />
             <label>Возраст</label>
             <input
               type="number"
               step="1"
-              value={inputAge}
-              onChange={ageChangeHandler}
+              ref={inputAgeRef}
+              // value={inputAge}
+              // onChange={ageChangeHandler}
             />
             <Button type="submit">Добавить Пользователя</Button>
           </div>
